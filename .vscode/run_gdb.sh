3e9d4bfec8debd790a9abfaaee24fcda0df91223
Elaina <1463967532@qq.com>
2025-01-18 16:30:11 +0800

    feat(all)创建了仓库与docker支持

---
+++ .vscode/run_gdb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+run_option=$*
+
+source ./install/setup.bash
+
+if [ -f /usr/bin/gnome-terminal ]
+then
+    # gnome-terminal -- bash -c " ros2 run --prefix 'gdbserver localhost:3038' $run_option"
+    gnome-terminal -- bash -c " ros2 run --prefix 'gdb -ex run --args' $run_option"
+else
+    if [ -f /usr/bin/konsole ]
+    then
+        konsole -e "ros2 run --prefix  'gdbserver localhost:3038' $run_option"
+    fi
+fi
