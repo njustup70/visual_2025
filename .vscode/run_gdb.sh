700d44e1f1f200c2a53da7126208d119444a0cf5
Elaina <1463967532@qq.com>
2025-01-19 16:12:23 +0000

    将debug启动从gdb更换成gdbsever并取消了在构建debug时候重新构建

--- .vscode/run_gdb.sh
+++ .vscode/run_gdb.sh
@@ -6,8 +6,8 @@ source ./install/setup.bash
 
 if [ -f /usr/bin/gnome-terminal ]
 then
-    # gnome-terminal -- bash -c " ros2 run --prefix 'gdbserver localhost:3038' $run_option"
-    gnome-terminal -- bash -c " ros2 run --prefix 'gdb -ex run --args' $run_option"
+    gnome-terminal -- bash -c " ros2 run --prefix 'gdbserver localhost:3038' $run_option"
+    # gnome-terminal -- bash -c " ros2 run --prefix 'gdb -ex run --args' $run_option"
 else
     if [ -f /usr/bin/konsole ]
     then
