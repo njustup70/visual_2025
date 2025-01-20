6edf424f9a4693a1d597137df834f33a9b12e180
Elaina <1463967532@qq.com>
2025-01-20 05:08:03 +0000

    fix(vscode)通过添加problemMatcher解决vscode认为不了后台任务完成的bug

--- .vscode/run_gdb.sh
+++ .vscode/run_gdb.sh
@@ -4,13 +4,14 @@ run_option=$*
 
 source ./install/setup.bash
 
-if [ -f /usr/bin/gnome-terminal ]
-then
-    gnome-terminal -- bash -c " ros2 run --prefix 'gdbserver localhost:3038' $run_option"
-    # gnome-terminal -- bash -c " ros2 run --prefix 'gdb -ex run --args' $run_option"
-else
-    if [ -f /usr/bin/konsole ]
-    then
-        konsole -e "ros2 run --prefix  'gdbserver localhost:3038' $run_option"
-    fi
-fi
+ros2 run --prefix 'gdbserver localhost:3038' $run_option 
+# if [ -f /usr/bin/gnome-terminal ]
+# then
+#     gnome-terminal -- bash -c " ros2 run --prefix 'gdbserver localhost:3038' $run_option"
+#     # gnome-terminal -- bash -c " ros2 run --prefix 'gdb -ex run --args' $run_option"
+# else
+#     if [ -f /usr/bin/konsole ]
+#     then
+#         konsole -e "ros2 run --prefix  'gdbserver localhost:3038' $run_option"
+#     fi
+# fi
