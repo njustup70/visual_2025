0b6f957354f83a88a78962bac7ee3f7854fa23a7
chaffer-cold <1463967532@qq.com>
2025-01-24 02:42:36 +0000

    feat(perception)将点云转雷达的范围变成-PI到PI

--- src/my_perception/launch/pointcloud_to_laserscan.launch.py
+++ src/my_perception/launch/pointcloud_to_laserscan.launch.py
@@ -19,8 +19,8 @@ def generate_launch_description():
                 'transform_tolerance': 0.1,
                 'min_height': -1.0,
                 'max_height': 1.0,
-                'angle_min': -1.5708,  # -M_PI/2
-                'angle_max': 1.5708,  # M_PI/2
+                'angle_min': -3.14159,  # -M_PI/2
+                'angle_max': 3.14159,  # M_PI/2
                 'angle_increment': 0.0087,  # M_PI/360.0
                 'scan_time': 0.3333,
                 'range_min': 0.4,
