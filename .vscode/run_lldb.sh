e593a3dcb86a533cd60666647c9fa9e38325cdfb
Elaina <1463967532@qq.com>
2025-02-26 07:26:33 +0000

    fix(vscode)添加了lldb调试插件,并通过在lldb 的sh中输出任意字符跳过vscode阻塞prelaunchtask

--- .vscode/run_lldb.sh
+++ .vscode/run_lldb.sh
@@ -3,6 +3,7 @@
 run_option=$*
 
 source ./install/setup.bash
+echo "started lldb-server on port 3039"
 ros2 run --prefix 'lldb-server-19 platform --server --listen *:3039' $run_option
 # if [ -f /usr/bin/gnome-terminal ]
 # then
