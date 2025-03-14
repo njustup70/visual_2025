21afdd668e4f2ceac9d36c128729652d2345cc6e
Elaina <1463967532@qq.com>
2025-03-14 09:38:40 +0000

    feat(nav2)添加了空地图

--- src/rc_navigation/launch/rc_nav.launch.py
+++ src/rc_navigation/launch/rc_nav.launch.py
@@ -15,6 +15,7 @@ def generate_launch_description():
     ld.add_action(DeclareLaunchArgument('params_file',default_value=param_file_path,description='Full path to the ROS2 parameters file to use'))
     ld.add_action(DeclareLaunchArgument('autostart', default_value='True', description='Automatically startup the nav2 stack'))
     #======调用原始launch文件，传入参数======
+    my_packager_share_dir = get_package_share_directory('rc_navigation')
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     nav2_bringup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([nav2_bringup_dir, '/launch', '/navigation_launch.py']),
@@ -22,5 +23,14 @@ def generate_launch_description():
                           'params_file': LaunchConfiguration('params_file')
                           }.items()
     )
+    #======================================启动地图服务
+    map_path=os.path.join(my_packager_share_dir,'map','empty_map.yaml')
+    ld.add_action(DeclareLaunchArgument('map', default_value=map_path, description='Full path to map yaml file to load'))
+    map_server_launch=IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([my_packager_share_dir,'/launch','/map_server.launch.py']),
+        launch_arguments={'use_sim_time': LaunchConfiguration('use_sim_time'),
+                          'map': LaunchConfiguration('map')}.items()
+    )
     ld.add_action(nav2_bringup_launch)
+    ld.add_action(map_server_launch)
     return ld
