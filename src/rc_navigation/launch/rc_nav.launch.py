90487666799ce90d529ac688f2a4d933cda2ad69
Hu Chuyi <923110800623@njust.edu.cn>
2025-04-21 15:53:55 +0000

    feat(nav)更改参数为实际参数,tf时间有问题

--- src/rc_navigation/launch/rc_nav.launch.py
+++ src/rc_navigation/launch/rc_nav.launch.py
@@ -10,7 +10,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld=LaunchDescription()
     param_file_path=os.path.join(get_package_share_directory('rc_navigation'),'config','nav2_params.yaml')
-    ld.add_action(DeclareLaunchArgument('use_sim_time', default_value='True',description='Use simulation clock'))
+    ld.add_action(DeclareLaunchArgument('use_sim_time', default_value='false',description='Use simulation clock'))
     ld.add_action(DeclareLaunchArgument('use_composition',default_value='False',description='Use lifecycle nodes'))
     ld.add_action(DeclareLaunchArgument('params_file',default_value=param_file_path,description='Full path to the ROS2 parameters file to use'))
     ld.add_action(DeclareLaunchArgument('autostart', default_value='True', description='Automatically startup the nav2 stack'))
