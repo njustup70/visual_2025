6c17784fcf15ae6d6ab2f3f8235e77a9dd0c63a8
Elaina <1463967532@qq.com>
2025-04-28 10:56:49 +0800

    Revert "feat(bringup)将nav启动改成自己的launch并启动组合节点"
    This reverts commit 8a35ef7025d63abf0a8909cf0f337259d889556a.

--- src/rc_navigation/launch/rc_nav.launch.py
+++ src/rc_navigation/launch/rc_nav.launch.py
@@ -11,14 +11,14 @@ def generate_launch_description():
     ld=LaunchDescription()
     param_file_path=os.path.join(get_package_share_directory('rc_navigation'),'config','nav2_params.yaml')
     ld.add_action(DeclareLaunchArgument('use_sim_time', default_value='false',description='Use simulation clock'))
-    ld.add_action(DeclareLaunchArgument('use_composition',default_value='True',description='Use lifecycle nodes'))
+    ld.add_action(DeclareLaunchArgument('use_composition',default_value='False',description='Use lifecycle nodes'))
     ld.add_action(DeclareLaunchArgument('params_file',default_value=param_file_path,description='Full path to the ROS2 parameters file to use'))
     ld.add_action(DeclareLaunchArgument('autostart', default_value='True', description='Automatically startup the nav2 stack'))
     #======调用原始launch文件，传入参数======
     my_packager_share_dir = get_package_share_directory('rc_navigation')
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     nav2_bringup_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([my_packager_share_dir, '/launch', '/navigation.launch.py']),
+        PythonLaunchDescriptionSource([nav2_bringup_dir, '/launch', '/navigation_launch.py']),
         launch_arguments={'use_sim_time': LaunchConfiguration('use_sim_time'),
                           'params_file': LaunchConfiguration('params_file')
                           }.items()
