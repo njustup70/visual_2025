12ac124636dc82424a39e29433921e78de982796
Elaina <1463967532@qq.com>
2025-04-30 15:21:41 +0000

    feat 将docker 改成测试镜像

--- .devcontainer/build.bash
+++ .devcontainer/build.bash
@@ -7,7 +7,8 @@ SCRIPT_DIR=$(dirname "$0")
 docker build -t elaina/visual "$SCRIPT_DIR"
 #如果传入 --github-action参数,则执行以下命令
 if [[ "$1" == "--github-action" ]]; then
-    docker build -t ghcr.io/elaina/visual:latest "$SCRIPT_DIR" 
+    # docker build -t ghcr.io/elaina/visual:latest "$SCRIPT_DIR" 
+    docker build -t ghcr.io/elaina/test:latest "$SCRIPT_DIR" -f "$SCRIPT_DIR/test.Dockerfile"
     #推送
     docker push ghcr.io/elaina/visual:latest
 fi
