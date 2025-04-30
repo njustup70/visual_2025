d6a51e04b20e9fc0a1e4ed4dae5da5f587e4ecfe
Elaina <1463967532@qq.com>
2025-04-30 15:25:23 +0000

    fix 修复bash语法问题

--- .devcontainer/build.bash
+++ .devcontainer/build.bash
@@ -2,14 +2,12 @@
 
 # 获取脚本所在目录
 SCRIPT_DIR=$(dirname "$0")
-#如果传入 --github-action参数,则执行以下命令
+
+# 如果传入 --github-action 参数
 if [[ "$1" == "--github-action" ]]; then
-    # docker build -t ghcr.io/elaina/visual:latest "$SCRIPT_DIR" 
     docker build -t ghcr.io/elaina/test:latest "$SCRIPT_DIR" -f "$SCRIPT_DIR/test.Dockerfile"
-    #推送
     docker push ghcr.io/elaina/visual:latest
-fi
 else
-    # 如果没有传入 --github-action参数,则执行以下命令
-    docker build -t elaina/visual:latest "$SCRIPT_DIR" 
+    # 如果没有传入 --github-action 参数
+    docker build -t elaina/visual:latest "$SCRIPT_DIR"
 fi
