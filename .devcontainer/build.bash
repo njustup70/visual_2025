0d284e1341b5efbaaf4d0de7da97f30a82fe0c64
Elaina <1463967532@qq.com>
2025-05-01 00:20:11 +0800

    修改镜像名称

--- .devcontainer/build.bash
+++ .devcontainer/build.bash
@@ -6,8 +6,8 @@ SCRIPT_DIR=$(dirname "$0")
 # 如果传入 --github-action 参数
 if [[ "$1" == "--github-action" ]]; then
     docker info
-    docker build -t elainasuki/rc2025/test:latest "$SCRIPT_DIR" -f "$SCRIPT_DIR/test.Dockerfile"
-    docker push elainasuki/rc2025/test:latest
+    docker build -t elainasuki/rc2025:test "$SCRIPT_DIR" -f "$SCRIPT_DIR/test.Dockerfile"
+    docker push elainasuki/rc2025:test
 else
     # 如果没有传入 --github-action 参数
     docker build -t elaina/visual:latest "$SCRIPT_DIR"
