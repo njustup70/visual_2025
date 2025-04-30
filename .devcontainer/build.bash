96bfc66d303627fb69e7c7da11a5d5336b6aa6dc
Elaina <1463967532@qq.com>
2025-04-30 15:24:19 +0000

    feat 将是否传入github-action分开

--- .devcontainer/build.bash
+++ .devcontainer/build.bash
@@ -2,13 +2,14 @@
 
 # 获取脚本所在目录
 SCRIPT_DIR=$(dirname "$0")
-
-# 构建 Docker 镜像，基于脚本所在目录（$SCRIPT_DIR）
-docker build -t elaina/visual "$SCRIPT_DIR"
 #如果传入 --github-action参数,则执行以下命令
 if [[ "$1" == "--github-action" ]]; then
     # docker build -t ghcr.io/elaina/visual:latest "$SCRIPT_DIR" 
     docker build -t ghcr.io/elaina/test:latest "$SCRIPT_DIR" -f "$SCRIPT_DIR/test.Dockerfile"
     #推送
     docker push ghcr.io/elaina/visual:latest
+fi
+else
+    # 如果没有传入 --github-action参数,则执行以下命令
+    docker build -t elaina/visual:latest "$SCRIPT_DIR" 
 fi
