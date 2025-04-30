94e3d4c669edf0863ddc61a298234b7ddcf7cdde
Elaina <1463967532@qq.com>
2025-04-30 15:15:21 +0000

    feat build脚本增加github-action支持

--- .devcontainer/build.bash
+++ .devcontainer/build.bash
@@ -5,3 +5,9 @@ SCRIPT_DIR=$(dirname "$0")
 
 # 构建 Docker 镜像，基于脚本所在目录（$SCRIPT_DIR）
 docker build -t elaina/visual "$SCRIPT_DIR"
+#如果传入 --github-action参数,则执行以下命令
+if [[ "$1" == "--github-action" ]]; then
+    docker build -t ghcr.io/elaina/visual:latest "$SCRIPT_DIR" 
+    #推送
+    docker push ghcr.io/elaina/visual:latest
+fi
