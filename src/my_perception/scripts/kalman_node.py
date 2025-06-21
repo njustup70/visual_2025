2be92059459b2103031ab923cff778b821d72ef3
Elaina <1463967532@qq.com>
2025-06-21 13:44:42 +0000

    refactor 更新X 将yaw 更改成为w z 避免更新出突变问题

--- src/my_perception/scripts/kalman_node.py
+++ src/my_perception/scripts/kalman_node.py
@@ -29,28 +29,28 @@ class KalmanNode(Node):
 
         # 状态向量 [x, y, yaw]
         # self.kf = KalmanFilter(dim_x=8, dim_z=8)
-        self.kf = FlexibleKalmanFilter(dim_x=8)
-        self.kf.x = np.zeros((8, 1))  # 默认初始化为0向量
+        self.kf = FlexibleKalmanFilter(dim_x=9)
+        self.kf.x = np.zeros((9, 1))  # 默认初始化为0向量
         
         # 构建状态转移矩阵
-        # 状态向量: {s} [px, py, theta, vx, vy, omega,ax,ay]
+        # 状态向量: {s} [px, py, theta, vx, vy, omega,ax,ay] 直接使用theta会有突变问题
+        # 状态向量: {s} [px, py, z,w, vx, vy, omega,ax,ay]
         dt = self.dt
-        dt2 = 0.5 * dt * dt 
-        self.F = np.array([
-            [1, 0, 0, dt, 0, 0, 0, 0],
-            [0, 1, 0, 0, dt, 0, 0, 0],
-            [0, 0, 1, 0, 0, dt, 0, 0],
-            [0, 0, 0, 1, 0, 0, dt, 0],
-            [0, 0, 0, 0, 1, 0, 0, dt],
-            [0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1],
-        ])
-        self.kf.F = np.copy(self.F)
+        # self.F = np.array([
+        #     [1, 0, 0, dt, 0, 0, 0, 0],
+        #     [0, 1, 0, 0, dt, 0, 0, 0],
+        #     [0, 0, 1, 0, 0, dt, 0, 0],
+        #     [0, 0, 0, 1, 0, 0, dt, 0],
+        #     [0, 0, 0, 0, 1, 0, 0, dt],
+        #     [0, 0, 0, 0, 0, 1, 0, 0],
+        #     [0, 0, 0, 0, 0, 0, 1, 0],
+        #     [0, 0, 0, 0, 0, 0, 0, 1],
+        # ])
+        # self.kf.F = np.copy(self.F)
         
         # 控制输入向量：{b}[vx,vy,vyaw]
         # 输入向量为命令输入
-        self.B= np.zeros((8, 3))
+        self.B= np.zeros((9, 3))
         self.B[3, 0] = 1.0  # vx_cmd -> vx
         self.B[4, 1] = 1.0  # vy_cmd -> vy
         self.B[2, 2] = dt    # vyaw_cmd -> theta (通过积分)
@@ -58,29 +58,25 @@ class KalmanNode(Node):
         # self.kf.B = self.B
         
         # 测量矩阵 - 组合版本
-        self.H = np.identity(8)
-        self.H[3,3]=0
-        self.H[4,4]=0
-        self.kf.H = self.H
-        self.H_imu= np.zeros((3, 8))
-        self.H_imu[[0,1,2],[6,7,5]] = 1.0  # IMU测量 [ax, ay, vyaw] -> [ax, ay, omega]
-        self.H_tf = np.zeros((3, 8))
-        self.H_tf[[0,1,2],[0,1,2]] = 1.0  # TF测量 [px, py, yaw] -> [px, py, yaw]
+        self.H_imu= np.zeros((3, 9))
+        self.H_imu[[0,1,2],[7,8,6]] = 1.0  # IMU测量 [ax, ay, vyaw] -> [ax, ay, omega]
+        self.H_tf = np.zeros((4, 9))
+        self.H_tf[[0,1,2,3],[0,1,2,3]] = 1.0  # TF测量 [px, py, z,w] -> [px, py, z ,w]
         # 过程噪声协方差矩阵
-        self.kf.Q = np.diag([0.001, 0.001, 0.006, 0.01, 0.01, 0.01,0.01, 0.01])
+        self.kf.Q = np.diag([0.001, 0.001, 0.006,0.006, 0.01, 0.01, 0.01,0.01, 0.01])
         # 测量噪声协方差矩阵
         self.R = np.diag([0.001, 0.001, 0.08, 0.4, 0.4, 0.01 ,0.001, 0.001])
         
         # 测量噪声协方差矩阵（根据传感器精度调整）
-        self.R_tf = np.diag([1.0, 1.0, 1.0])  # TF测量噪声（x,y,yaw）
+        self.R_tf = np.diag([1.0, 1.0, 0.2,0.2])  # TF测量噪声（x,y,z,w）
         self.R_imu = np.diag([1.0, 1.0, 1.0])    # IMU测量噪声（ax,ay,ayaw）
         
         # 初始估计误差协方差
-        self.kf.P = np.diag([0.1, 0.1, 0.01, 0.5, 0.5, 0.1, 1.0, 1.0])
+        self.kf.P = np.diag([0.1, 0.1, 0.01,0.01, 0.5, 0.5, 0.1, 1.0, 1.0])
         
         # 控制输入和测量缓存
         self.cmd_vel = np.zeros(3)  # 控制输入[vx, vy, vyaw]
-        self.odom = np.zeros(3)     # [x, y, yaw]
+        self.odom = np.zeros(5)     # [x, y, yaw,z,w]
         self.imu_data = [0.0,0.0,0.0]
         # self.imu_data = np.zeros(3) # [ax, ay, ayaw]
         
@@ -117,8 +113,10 @@ class KalmanNode(Node):
         self.odom[2] = self.get_yaw_from_quaternion(
             rotation.x, rotation.y, rotation.z, rotation.w
         )
+        self.odom[3]= rotation.z  # 添加z坐标
+        self.odom[4] = rotation.w  # 添加四元数w
         #现构造行再转化成列向量
-        z=np.array([self.odom[0], self.odom[1], self.odom[2]]).reshape(-1, 1)  # 简化后
+        z=np.array([self.odom[0], self.odom[1], self.odom[3],self.odom[4]]).reshape(-1, 1)  # 简化后
         # self.kf.update(z)
         self.kf.update(z, H=self.H_tf, R=self.R_tf)
         # 执行基于TF的更新
@@ -144,8 +142,9 @@ class KalmanNode(Node):
         self.last_time = current_time
         
         # dt = self.dt  # 实际时间差
-        yaw=self.kf.x[2, 0]  # 获取当前yaw角
-    
+        # yaw=self.kf.x[2, 0]  # 获取当前yaw角
+        yaw=self.get_yaw_from_quaternion(0,0,self.kf.x[2, 0],self.kf.x[3, 0])
+        #状态量为 [px, py, z, w, vx, vy, omega, ax, ay]
         F=np.array([
             [1, 0, 0, dt*np.cos(yaw), -dt*np.sin(yaw), 0, 0, 0],
             [0, 1, 0, dt*np.sin(yaw), dt*np.cos(yaw), 0, 0, 0],
@@ -156,8 +155,18 @@ class KalmanNode(Node):
             [0, 0, 0, 0, 0, 0, 1, 0],
             [0, 0, 0, 0, 0, 0, 0, 1],
         ])
-        self.kf.F = F # 更新状态转移矩阵
+        F=np.eye(9)
+        F[0, 4] = dt * np.cos(yaw) #x=x+vx*cos(yaw)*dt-vx*sin(yaw)*dt
+        F[0, 5] = -dt * np.sin(yaw)
+        F[1, 4] = dt * np.sin(yaw) #y=y+vx*sin(yaw)*dt+vx*cos(yaw)*dt
+        F[1, 5] = dt * np.cos(yaw)
+        F[2, 6] = dt*0.5
+        F[3, 6] = dt*-0.5
+        #z=z+0.5*w*omega*dt
+        #w=w-0.5*z*omega*dt
 
+        self.kf.F = F # 更新状态转移矩阵
+       
         # 执行预测步骤（考虑控制输入）
         vx = self.cmd_vel[0]
         vy = self.cmd_vel[1]
@@ -188,8 +197,10 @@ class KalmanNode(Node):
         tf_pub.transform.translation.z = 0.0
         tf_pub.transform.rotation.x = 0.0
         tf_pub.transform.rotation.y = 0.0
-        tf_pub.transform.rotation.z = math.sin(self.kf.x[2, 0] / 2.0)
-        tf_pub.transform.rotation.w = math.cos(self.kf.x[2, 0] / 2.0)
+        tf_pub.transform.rotation.z=self.kf.x[2, 0]  # 使用z
+        tf_pub.transform.rotation.w=self.kf.x[3, 0]  # 使用w
+        # tf_pub.transform.rotation.z = math.sin(self.kf.x[2, 0] / 2.0)
+        # tf_pub.transform.rotation.w = math.cos(self.kf.x[2, 0] / 2.0)
         self.tf_broadcaster.sendTransform(tf_pub)
         
         # 这里可以添加发布融合后状态的代码
